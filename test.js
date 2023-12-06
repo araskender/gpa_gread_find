950447731b117cb41f49cdcc99ee02c875cac205
Md Ashiqur Rahman <38679297+araskender@users.noreply.github.com>
2023-12-06 18:52:40 +0600

    Add files via upload

---
+++ test.js
@@ -0,0 +1,25 @@
+var marks = prompt("Enter your marks :");
+
+if(marks>=101 || marks <=0)
+    document.write("Invalid Input");
+
+else if (marks>=80 && marks<=100)
+document.write("You have got A+.");
+
+else if (marks>=70 && marks<=79)
+document.write("You have got A.");
+
+else if (marks>=60 && marks<=69)
+document.write("You have got A-.");
+
+else if (marks>=50 && marks<=59)
+document.write("You have got B.");
+
+else if (marks>=40 && marks<=49)
+document.write("You have got C.");
+
+else if (marks>=33 && marks<=39)
+document.write("You have got D.");
+
+else
+document.write("Your Fail.");
